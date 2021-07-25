465265da95ca48c15bb65bf91659143c3f74e000
Knut Ahlers <knut@ahlers.me>
2021-07-25 13:43:19 +0200

    Add support for Duration, add empty values
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- helpers.go
+++ helpers.go
@@ -1,4 +1,12 @@
 package main
 
-func ptrInt64(v int64) *int64    { return &v }
-func ptrString(v string) *string { return &v }
+import "time"
+
+var (
+	ptrInt64Zero   = ptrInt64(0)
+	ptrStringEmpty = ptrString("")
+)
+
+func ptrDuration(v time.Duration) *time.Duration { return &v }
+func ptrInt64(v int64) *int64                    { return &v }
+func ptrString(v string) *string                 { return &v }
